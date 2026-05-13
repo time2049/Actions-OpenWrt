cb89c7ae347949eecf6c5bd2739f30259a27aace
time2049 <58364786+time2049@users.noreply.github.com>
2026-05-13 16:12:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
+# 修改默认管理 IP
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 强制分区大小（确保 .config 中已设置，但为防被覆盖）
+sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=256/' .config
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=2048/' .config
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 确保 OpenClash 和 PassWall 已选中（如果 .config 没有，则追加）
+for pkg in luci-app-openclash luci-app-passwall; do
+    if ! grep -q "CONFIG_PACKAGE_${pkg}=y" .config; then
+        echo "CONFIG_PACKAGE_${pkg}=y" >> .config
+    fi
+done
+
+# 运行 defconfig 使配置生效
+make defconfig
