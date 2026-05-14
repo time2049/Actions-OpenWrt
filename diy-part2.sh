842c5bcd9eb4bf753f0a32ecece420e0af8a9c0e
time2049 <58364786+time2049@users.noreply.github.com>
2026-05-14 15:38:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,14 +4,13 @@
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
 # --- 将 Argon 设为默认主题 ---
-# 1. 修改 luci-light Makefile (针对新版 OpenWrt)
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
-
-# 2. (可选) 同时修改其他几个 Makefile 以确保兼容性
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-# --- 设置完毕 ---
+
+# --- 设置 LuCI 默认语言为简体中文 ---
+sed -i "s/option lang 'auto'/option lang 'zh-cn'/" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # 运行 defconfig 使配置生效
 make defconfig
