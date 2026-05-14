cff56b6b2f264079607d46bccbefc263f498e904
time2049 <58364786+time2049@users.noreply.github.com>
2026-05-15 07:01:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 1. 添加 PassWall 源 (使用 openwrt-25.12 分支，与稳定版兼容)
-echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;openwrt-25.12' >> feeds.conf.default
-echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;openwrt-25.12' >> feeds.conf.default
+# 添加 PassWall 源（开发版使用 main 分支）
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
 
-# 2. 添加 OpenClash 源 (主分支)
+# 添加 OpenClash 源
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
-# 3. 添加 Argon 主题源 (主分支)
+# 添加 Argon 主题源
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
 
-# 4. Argon 配置插件 (与 OpenWrt 25.12 不兼容，暂时禁用)
-# echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config.git' >> feeds.conf.default
+# 添加 Argon 配置插件（开发版兼容）
+echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config.git' >> feeds.conf.default
