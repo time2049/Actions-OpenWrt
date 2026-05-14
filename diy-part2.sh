e6dfe4457231c7003e3946cc24e5370020d0d3b8
time2049 <58364786+time2049@users.noreply.github.com>
2026-05-14 15:34:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,5 +3,15 @@
 # 修改默认 IP
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
-# 可选：运行 defconfig（工作流后续也会执行，这里可省略，但保留也无妨）
+# --- 将 Argon 设为默认主题 ---
+# 1. 修改 luci-light Makefile (针对新版 OpenWrt)
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+
+# 2. (可选) 同时修改其他几个 Makefile 以确保兼容性
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+# --- 设置完毕 ---
+
+# 运行 defconfig 使配置生效
 make defconfig
