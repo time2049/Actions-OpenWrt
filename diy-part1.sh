2e75bca4d5d89c018b239ba40b125f0e52a30edc
time2049 <58364786+time2049@users.noreply.github.com>
2026-05-15 11:31:37 +0800

    Improve comments and fix package cleanup in script
    Updated comments for clarity and added a fix note for cleaning up conflicting packages.

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,10 +7,10 @@ sed -i '$a src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-pas
 # 添加 OpenClash 源
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git' feeds.conf.default
 
-# 添加 Argon 主题及其配置
+# 添加 Argon 主题及其配置插件
 sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
 sed -i '$a src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config.git' feeds.conf.default
 
-# 预先清理冲突包，强制 feeds update 时重新拉取
+# 【修复】预先清理冲突包，强制 feeds 刷新时重新拉取最新源码
 rm -rf package/feeds/passwall_packages/shadowsocksr-libev
 rm -rf package/feeds/passwall_packages/v2ray-geodata
